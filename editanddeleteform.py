8392ce08a51ab24f226b2076deef7d786379d81c
Pimenov Mikhail <pimenov57@gmail.com>
2020-06-13 16:55:36 +0600

    13.06.2020 Небольшие изменения в размещении элементов интерфейса формы удаления записи из БД

--- editanddeleteform.py
+++ editanddeleteform.py
@@ -7,28 +7,29 @@ class Window_one (QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self, parent)
 
         self.label1 = QtWidgets.QLabel ('Кого ищем?', self)
-        self.label1.setGeometry(10,10,280,60)
+        self.label1.setGeometry(10,10,70,20)
 
         self.foundtext_line = QtWidgets.QLineEdit (self)
-        self.foundtext_line.resize(500,50)
-        self.foundtext_line.move(10,70)
+        self.foundtext_line.resize(280,25)
+        self.foundtext_line.move(10,30)
 
         self.found_button = QtWidgets.QPushButton('Поиск ', self)
-        self.found_button.resize(200,40)
-        self.found_button.move(150,130)
+        self.found_button.resize(100,30)
+        self.found_button.move(40,70)
         self.found_button.clicked.connect(self.found_clicked)
 
         self.delete_button = QtWidgets.QPushButton('Удаление', self)
-        self.delete_button.resize(200,40)
-        self.delete_button.move(150,180)
+        self.delete_button.resize(100,30)
+        self.delete_button.move(170,70)
         self.found_button.clicked.connect(self.delete_clicked)
 
         self.tablist = QtWidgets.QTableWidget(self)
-        self.tablist.setGeometry (280,25,700,610)
+        self.tablist.setGeometry (300,10,500,100)
         self.tablist.setColumnCount(3)
         self.tablist.setSortingEnabled(True)
         self.column_label = ['ID','Ф.И.О','Серия и N паспорта']
         self.tablist.setHorizontalHeaderLabels(self.column_label)
+        self.tablist.resizeColumnToContents(0)
         self.tablist.resizeColumnToContents(1)
         self.tablist.resizeColumnToContents(2)
         self.tablist.insertRow(self.tablist.rowCount())
@@ -42,7 +43,7 @@ class Window_one (QtWidgets.QWidget):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     form1 = Window_one()
-    form1.setFixedSize(1000,700)
+    form1.setFixedSize(810,130)
     form1.setWindowModality(1)
     form1.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
     form1.setWindowTitle('Удаление записи')
